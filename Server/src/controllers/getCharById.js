7222684317ad49a76686742f9ba83c1da7d97c8d
MCalderonTarazona <mact.11@gmail.com>
2023-04-06 15:43:43 -0500

    Deploy Server/Client

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+const getCharById = (res,id) => {
+    axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(({data}) => {
+    console.log(data)
+    const {id, name, gender, species, origin, image, status } = data;
+    res.writeHead(200,{"Content-Type": "application/json"});
+    res.end(JSON.stringify({id, name, gender, species, origin, image, status }));
+    })
+    .catch((reason) => {
+    res.writeHead(500,{"Content-Type": "text/plain"});
+    res.end(reason.message);
+    })
+}
+
+module.exports = getCharById;
